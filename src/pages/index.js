4ef78c034816222589eb664c0fe18753ef82811b
Akarshan Yadav <akarshanyadav433@gmail.com>
2024-09-05 00:49:04 +0530

    Initial files

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import Login from "./Login";
+import Register from "./Register";
+import Profile from "./Profile";
+import ResetPassword from "./ResetPassword";
+
+export {HomePage,Login,Register,Profile,ResetPassword};
